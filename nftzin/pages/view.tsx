867880d0a206185c8b72636029b9b8c79ed41c61
Lucas Oliveira <olivmath@protonmail.com>
2024-03-22 11:28:13 -0300

    add: logic to redirect pages based on connect/disconnect wallte

--- nftzin/pages/view.tsx
+++ nftzin/pages/view.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { useAccount } from 'wagmi';
+import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 
 const ViewPage: NextPage = () => {
+  const { isConnected } = useAccount();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isConnected) {
+      router.push("/");
+    }
+  }, [isConnected, router]);
+
   return (
     <div
       style={{
