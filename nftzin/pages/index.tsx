4a713fc96260370533069b45636437ec89b99dc8
Lucas Oliveira <olivmath@protonmail.com>
2024-03-22 09:42:12 -0300

    update: home

--- nftzin/pages/index.tsx
+++ nftzin/pages/index.tsx
@@ -3,15 +3,28 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        padding: 12,
-      }}
-    >
-      <ConnectButton />
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          padding: 12,
+        }}
+      >
+        <ConnectButton />
+      </div>
+      <div
+        style={{
+          fontSize: "240px",
+          fontWeight: "bold",
+          marginTop: "120px",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <main>NFTzin</main>
+      </div>
+    </>
   );
 };
 
